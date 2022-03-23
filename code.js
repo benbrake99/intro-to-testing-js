93f0944d16255104ef7704c1aa149a19fa9b980d
Ben Brake <ben.brake99@gmail.com>
2022-03-23 12:05:23 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,21 @@
 // helloWorld function 
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// function statement syntax
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// // function expression syntax (assigning an anonymous function to a variable)
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
+}
+function sayHello(){
+    return "sayHello";
 }
--- tests.js
+++ tests.js
@@ -13,3 +13,11 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+//
+
+//Next function
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+});
